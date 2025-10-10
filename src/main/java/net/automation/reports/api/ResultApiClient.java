126ef9ca6187c7c8409189a3c713fb37c22f39b3
rahulagarwal <rahul.agarwal@wisetechglobal.com>
2025-10-10 15:51:13 +0530

    changes in result api client

--- src/main/java/net/automation/reports/api/ResultApiClient.java
+++ src/main/java/net/automation/reports/api/ResultApiClient.java
@@ -52,7 +52,7 @@ public class ResultApiClient {
 
         TestRun payloadTest = new TestRun()
                 .setDatetimeStart(report.getStart().toInstant(ZoneOffset.UTC))
-                .setServer(System.getProperty(BUILD_ENVIRONMENT, "Demo"))
+                .setServer(System.getProperty(BUILD_ENVIRONMENT, "QA"))
                 .setRunType(System.getProperty(RUN_TYPE, "Galileo"))
                 .setBranch(System.getProperty(RUN_BRANCH, "develop"))
                 .setBuildNumber(Integer.valueOf(System.getProperty(BUILD_NUMBER, "1")));
@@ -77,10 +77,10 @@ public class ResultApiClient {
 
         TestRun payload = new TestRun()
                 .setDatetimeStart(report.getStart().toInstant(ZoneOffset.UTC))
-                .setServer(System.getProperty(BUILD_ENVIRONMENT, "ad hoc"))
-                .setRunType(System.getProperty(RUN_TYPE, "ad hoc"))
-                .setBranch(System.getProperty(RUN_BRANCH, "ad hoc"))
-                .setBuildNumber(Integer.valueOf(System.getProperty(BUILD_NUMBER, "ad hoc")))
+                .setServer(System.getProperty(BUILD_ENVIRONMENT, "QA"))
+                .setRunType(System.getProperty(RUN_TYPE, "Galileo"))
+                .setBranch(System.getProperty(RUN_BRANCH, "develop"))
+                .setBuildNumber(Integer.valueOf(System.getProperty(BUILD_NUMBER, "1")))
                 .setParentRun(parentRun);
 
         return TypeHelper.convertToJson(payload);
